439cae02d78512a43d1cf3b8da88247a235cfbc3
Abhishek Mishra <abhishekmishra0513@gmail.com>
2025-04-16 16:14:20 +0530

    commit 03

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,21 @@
 package org.example;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
+public class App
+{
+    public static void main( String[] args ) throws LifecycleException {
+        System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+
+        Context context = tomcat.addContext("", null);
+        Tomcat.addServlet(context, "Servlet", new Servlet());
+        context.addServletMappingDecoded("/hello", "Servlet");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
--- src/main/java/org/example/Servlet.java
+++ src/main/java/org/example/Servlet.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+//@WebServlet("/hello")
 public class Servlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+        res.setContentType("text/html");
+        res.getWriter().println("<h2><b>Hello Abhishek</h2>");
     }
 }
